4feb24029f13f56555eb254124d219b3098d6890
Jim <jim.pollaro@gmail.com>
2017-12-11 10:24:04 -0800

    jim's commit to app-component.

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CharserviceService } from '../charservice.service'
 
 @Component({
   selector: 'app-select',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _charService: CharserviceService) { }
 
   ngOnInit() {
   }
