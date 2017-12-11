ef2ed2d327286b48dd599b410cc4e17064225a03
Jim <jim.pollaro@gmail.com>
2017-12-11 11:03:28 -0800

    Html of select component has all characters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SelectComponent } from './select/select.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', pathMatch: 'full', component: SelectComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -7,15 +7,17 @@ import { CharserviceService } from '../charservice.service'
   styleUrls: ['./select.component.css']
 })
 export class SelectComponent implements OnInit {
-  allchars = ['abigail','akuma','alex','balrog','birdie','cammy','chunli','dhalsim',
-  'ed','fang','guile','ibuki','juri','karin','ken','kolin','laura','mbison','menat','nash',
-  'necalli','r-mika','rashid','ryu','urien','vega','zangief','zeku']
 
-  
-  constructor(private _charService: CharserviceService) { }
-  
+    allchars = ['abigail','akuma','alex','balrog','birdie','cammy','chunli','dhalsim',
+    'ed','fang','guile','ibuki','juri','karin','ken','kolin','laura','mbison','menat','nash',
+    'necalli','r-mika','rashid','ryu','urien','vega','zangief','zeku']
 
-  ngOnInit() {
-  }
+    character = {}
+
+    constructor(private _charService: CharserviceService) { }
+
+
+    ngOnInit() {
+    }
 
 }
