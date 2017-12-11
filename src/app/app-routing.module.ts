b3f035e6b16d6ec2367cf74ea343440ba98e6a44
kwlee <keith.won.lee@gmail.com>
2017-12-11 13:21:40 -0800

    added some styling and about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SelectComponent } from './select/select.component'
+import { AboutComponent } from './about/about.component'
 
 const routes: Routes = [
-    { path: '', pathMatch: 'full', component: SelectComponent }
+    { path: '', pathMatch: 'full', component: SelectComponent },
+    { path:'about', pathMatch: 'full', component: AboutComponent }
 ];
 
 @NgModule({
